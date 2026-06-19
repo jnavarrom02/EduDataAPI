10a07fb593a868a166144e196baa60ef128b0020
Juan Daniel Navarro Monge <j.navarro02@hotmail.com>
2026-06-18 21:25:44 -0600

    Cambios de Conexion y API

--- Program.cs
+++ Program.cs
@@ -1,41 +1,42 @@
+using EduDataAPI;
+using EduDataAPI.Modelos;
+using EduDataAPI.Repositorios;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Controladores
+builder.Services.AddControllers();
+
+
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// Repositorios
+builder.Services.AddScoped<UsuarioRepository>();
+
+
+// OpenAPI
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Sin SSL
+//app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- obj/Debug/net10.0/EduDataAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/EduDataAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduDataAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef89e76b01450cc5745744e24d02857eca5d68b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduDataAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduDataAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
